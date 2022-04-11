615ee51de98dfdb0b308df7ad403645691c38461
RetaxMaster <retaxmaster@hotmail.com>
2022-04-11 18:20:30 -0500

    03 PHPDoc

---
+++ phpdoc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Esta función retorna la suma de dos números
+ *
+ * @param int $n1
+ * @param int $n2
+ * @return int
+ */
+function sumar($n1, $n2) {
+    return $n1 + $n2;
+}
+
+$resultado = sumar(1, 5);
+
+echo "\n";
