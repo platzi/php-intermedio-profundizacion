387740d61b4d46a05f646a6160e1c4e3ed0e5b4e
RetaxMaster <retaxmaster@hotmail.com>
2022-04-11 18:22:26 -0500

    11 Constantes

---
+++ constantes.php
@@ -0,0 +1,22 @@
+<?php
+
+/* $decision = true;
+
+if ($decision) {
+    //define("BLOCK_CONSTANT", "Esta constante fue declarada dentro de un if");
+
+    const BLOCK_CONSTANT = "Esta constante fue declarada dentro de un if";
+
+
+    echo BLOCK_CONSTANT;
+} */
+
+
+
+
+
+
+
+echo __LINE__;
+
+echo "\n";
