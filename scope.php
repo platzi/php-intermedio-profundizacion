2831e46b501240ced624a580418803dc2ad4b3cb
RetaxMaster <retaxmaster@hotmail.com>
2022-04-12 13:03:01 -0500

    13 Scope

---
+++ scope.php
@@ -0,0 +1,24 @@
+<?php
+
+/* $outside_variable = "Esto es una variable global";
+
+function my_function() {
+
+    global $outside_variable;
+    echo $outside_variable;
+
+}
+
+my_function(); */
+
+$outside_variable = "Esto es una variable global";
+
+function my_function() {
+
+    echo $GLOBALS["outside_variable"];
+
+}
+
+my_function(); 
+
+echo "\n";
