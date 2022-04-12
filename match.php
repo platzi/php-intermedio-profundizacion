80065bba0d489cfd76762ee759648c0e7c6b353d
RetaxMaster <retaxmaster@hotmail.com>
2022-04-12 16:05:43 -0500

    16 Match

---
+++ match.php
@@ -0,0 +1,42 @@
+<?php
+
+function get_country_name_switch($country) {
+
+    $name = "";
+
+    switch ($country) {
+        case 'MX':
+            $name = "México";
+            break;
+
+        case 'COL':
+            $name = "Colombia";
+            break;
+
+        case 'EUA':
+            $name = "Estados Unidos Americanos";
+            break;
+        
+        default:
+            $name = "Lo siento, no conozco ese país";
+            break;
+    }
+
+    return $name;
+    
+}
+
+function get_country_name_match($country) {
+
+    return match($country) {
+        "MX" => "México",
+        "COL" => "Colombia",
+        "EUA" => "Estados Unidos Americanos",
+        default => "Lo siento, no conozco ese país"
+    };
+
+}
+
+echo get_country_name_match("LKASJDKLASDNLAS");
+
+echo "\n";
